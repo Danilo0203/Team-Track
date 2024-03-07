8497b34af226eb5472c255b5825a000bf2a0a0c7
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-03-06 22:12:17 -0600

    Auto-generated commit

---
+++ chunk-KT5G3RB3.js
@@ -0,0 +1,1 @@
+import{i as a}from"./chunk-5YS55EUS.js";import"./chunk-CUTGF6DA.js";import"./chunk-V4VAVA2C.js";export{a as DashboardModule};
---
+++ chunk-ZDVOCGIO.js
@@ -0,0 +1,1 @@
+import{e as a}from"./chunk-CUTGF6DA.js";import"./chunk-V4VAVA2C.js";export{a as WidgetsModule};
